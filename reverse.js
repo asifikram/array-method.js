2fecbe26498d41f8f2c5386d8d0f19db1d808bae
asifikram <nazatiyan4339@gmail.com>
2026-02-25 11:33:34 +0600

    first commit

---
+++ reverse.js
@@ -0,0 +1,20 @@
+// const sentense = 'i am learning web'
+
+// let reverse = ''
+// for(letter of sentense){
+//     // console.log(letter)
+//     reverse = letter + reverse
+// }
+
+// console.log(reverse)
+
+const sentense = 'i am a student'
+
+let reverse = ''
+
+for(letter of sentense){
+    // console.log(letter)
+    reverse = letter + reverse
+}
+
+console.log(reverse)
