3114a3561de0400e958309253fd2d3cdaebd38c1
asifikram <nazatiyan4339@gmail.com>
2026-02-27 16:28:10 +0600

    first commit

---
+++ sort.js
@@ -0,0 +1,12 @@
+
+const person = ['akib', 'rakib', 'jawad', 'babu']
+const sortedPerson = person.sort()
+
+console.log(sortedPerson)
+
+const numbers = [1,2,34,67,98,54,4]
+const number_asc = [...numbers].sort(function(a,b){return a-b})
+const number_dsc = [...numbers].sort(function(a,b){return b-a})
+
+console.log(number_dsc)
+console.log(number_asc)
