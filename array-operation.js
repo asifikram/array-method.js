f4fb9091953ec4daa0e5abb6468d1ca57c4115fa
asifikram <nazatiyan4339@gmail.com>
2026-02-28 16:00:47 +0600

    first commit

---
+++ array-operation.js
@@ -0,0 +1,48 @@
+
+//get sum of all numbers by for of loop
+// function sumofnumbers (numbers){
+//     let sum = 0
+//     for(const number of numbers){
+//         console.log(number)
+//         sum = sum + number
+//     }
+//     return sum
+// }
+// const numbs = [11,32,12,45]
+// const sum = sumofnumbers(numbs)
+// console.log('the sum is',sum)
+
+
+
+
+// get sum of all numbers by for loop
+// function sumofnumbers(numbers){
+//     let sum = 0
+//     for(let i = 0; i<numbers.length;i++){
+//         console.log(numbers[i])
+//         sum = sum + numbers[i]
+//     }
+//     return sum
+// }
+// const numbs = [12,45,67,43]
+// const sum = sumofnumbers(numbs)
+// console.log('the sum is',sum)
+
+
+
+// get sum of all numbers by while loop
+// function addofnumbers (numbers){
+//     let i =0
+//     let sum = 0
+//     while(i<numbers.length){
+//         console.log(numbers[i])
+//         sum = sum + numbers[i]
+//         i++
+//     }
+//     return sum
+// }
+
+// const digit = [34,56,43,56,60]
+// const total = addofnumbers(digit)
+
+// console.log('the total is', total)
