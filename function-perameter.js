97a5c31bba899050556ee8e862a59384678c5b14
asifikram <nazatiyan4339@gmail.com>
2026-02-28 09:56:45 +0600

    first commit

---
+++ function-perameter.js
@@ -0,0 +1,22 @@
+
+// get square value by function
+// function square (number){
+//      console.log(number)
+//      const borgo = number * number
+//      console.log(borgo)
+// }
+
+// square(4)
+// console.log('-----')
+// console.log('-----')
+// console.log('-----')
+// square(26)
+
+// get add by function
+
+function add (num1, num2){
+   const number = num1 + num2
+   console.log(number)
+}
+
+add(5,6)
---
+++ function.js
@@ -0,0 +1,14 @@
+
+function teethBrus(){
+    console.log('take your brush')
+    console.log('add some paste')
+    console.log('then maja maji kore fena bahir koro')
+}
+
+teethBrus();
+console.log('-------')
+console.log('-------')
+console.log('-------')
+console.log('-------')
+console.log('-------')
+teethBrus();
