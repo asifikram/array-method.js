ba2faf9e82e8ad2c64ad18bf2792a89a67731d81
asifikram <nazatiyan4339@gmail.com>
2026-02-28 10:28:37 +0600

    first commit

---
+++ arguments.js
@@ -0,0 +1,29 @@
+
+function doubled(number){
+   const digon = number * 2
+   console.log(number,digon)
+}
+
+
+// doubled(34)
+// console.log('-------')
+// doubled(54)
+// console.log('-------')
+// doubled(654)
+// const number = 113
+// doubled(number)
+// const money = 5000
+// doubled(money)
+
+
+
+function diffrent ( num1, num2){
+    const diff = num1 - num2
+    console.log(num1, '-', num2, '=', diff)
+}
+
+diffrent(12, 7)
+const myage = 34
+const myfarherage = 67
+
+diffrent(myfarherage, myage)
